1a994a02e9724fab456127444f060a9fe2293855
Hafiz Muhammad OSAID Ahmed <149686768+hafizUsiad@users.noreply.github.com>
2025-05-30 09:34:25 +0500

    Update serverconfig.js

--- src/serverconfig.js
+++ src/serverconfig.js
@@ -1,4 +1,4 @@
-const server = "http://localhost:5000";
+const server = "https://hapestbackend-production.up.railway.app";
 const frontend_url = "http://localhost:3000";
 
 export {server,frontend_url};
